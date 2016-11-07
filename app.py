f80e3b61e5e600466f6c88ec65587c1770aeff8d
Belle Noonan <bellenoonan@Belles-MacBook-Pro-2.local>
2016-11-06 19:30:09 -0500

    Post data from serial port to flask app

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-curr_temp_info = 0
+curr_temp = 0
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return curr_temp
 
-@app.route('/temp_data')
+@app.route('/temp_data', methods=['GET', 'POST'])
 def temp_data():
     global curr_temp
-    if request.method == 'POST' and 'temp_dict' in request.form.keys():
-        curr_temp = request.form['temp_dict']
+    if request.method == 'POST' and 'degrees_f' in request.form.keys():
+        curr_temp = request.form['degrees_f']
         response = jsonify(request.form) # echo the received info back
         return response
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- read_data.py
+++ read_data.py
@@ -1,7 +1,7 @@
 import serial, requests
-ser = serial.Serial('/dev/cu.usbmodem1421', 9600)
+ser = serial.Serial('/dev/cu.usbmodem1411', 9600)
 
 while True:	
 	# r = requests.post('http://hackmit-spot.herokuapp.com/availability_data', data={'availability':ser.readline()[0]})
-	r = requests.post('http://127.0.0.1:5000/temp_data', data={'temp_dict':ser.readline()[0]})
+	r = requests.post('http://127.0.0.1:5000/temp_data', data={'degrees_f':ser.readline()})
 	print r.text
