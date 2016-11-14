c3334355c50abd020fb08b2e943451ec1338ea73
mantaleigh <samantha@samanthavoigt.com>
2016-11-14 13:52:04 -0500

    updates from tempest

--- app.py
+++ app.py
@@ -1,20 +1,28 @@
 from flask import Flask, request, jsonify
+import os, datetime
+
 app = Flask(__name__)
 
 curr_temp = 0
+time_changed = datetime.datetime.now()
 
 @app.route('/')
-def hello_world():
-    return curr_temp
+def home():
+    return "The current temperature in F is: " + str(curr_temp) + " and was updated at: " + str(time_changed)
 
 @app.route('/temp_data', methods=['GET', 'POST'])
 def temp_data():
     global curr_temp
+    global time_changed
     if request.method == 'POST' and 'degrees_f' in request.form.keys():
         curr_temp = request.form['degrees_f']
+        time_changed = datetime.datetime.now()
         response = jsonify(request.form) # echo the received info back
         return response
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.debug = True
+    port = 6024
+    # Flask will print the port anyhow, but let's do so too
+    print('Running on port '+str(port))
+    app.run('0.0.0.0',port)
