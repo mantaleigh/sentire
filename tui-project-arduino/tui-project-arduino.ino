cfcae1fc7a0a302ad8cc507d19dd773493188e4f
mantaleigh <samantha@samanthavoigt.com>
2016-12-07 17:10:09 -0500

    Modify arduino code to handle arbitrary pin assignments

--- tui-project-arduino/tui-project-arduino.ino
+++ tui-project-arduino/tui-project-arduino.ino
@@ -1,160 +1,138 @@
-/*
-  State change detection (edge detection)
-
- Often, you don't need to know the state of a digital input all the time,
- but you just need to know when the input changes from one state to another.
- For example, you want to know when a button goes from OFF to ON.  This is called
- state change detection, or edge detection.
-
- This example shows how to detect when a button or button changes from off to on
- and on to off.
-
- The circuit:
- * pushbutton attached to pin 2 from +5V
- * 10K resistor attached to pin 2 from ground
- * LED attached from pin 13 to ground (or use the built-in LED on
-   most Arduino boards)
-
- created  27 Sep 2005
- modified 30 Aug 2011
- by Tom Igoe
-
-This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/ButtonStateChange
-
+/* 
+ *  Sentire
+ *  Allow for reading of 3 arbitary analog sensors + 4 arbitrary digital inputs (buttons)
+ *  Use the on board LED as a second button click indicator
  */
 
-// pin assignments
-const int LDR = 0;
 
-// this constant won't change:
-const int  buttonPin_one = 2;    // the pin that the pushbutton is attached to
-const int  buttonPin_two = 3;
+// analog sensor pins -- these are read every minute (or whatever)
+const int a_1 = 1
+const int a_2 = 2
+const int a_3 = 3
+
+// digital sensor pins
+const int d_1 = 1
+const int d_2 = 2
+const int d_3 = 3
+const int d_4 = 4
+
 const int ledPin = 13;       // the pin that the LED is attached to
-const int temperaturePin = A1;
 
 // Variables will change:
-int buttonOnePushCounter = 0;   // counter for the number of button presses
-int buttonOneState = 0;         // current state of the button
-int lastButtonOneState = 0;     // previous state of the button
+int d_1_state = 0;         // current state of the button
+int last_d_1_state = 0;     // previous state of the button
 
-int buttonTwoPushCounter = 0;   // counter for the number of button presses
-int buttonTwoState = 0;         // current state of the button
-int lastButtonTwoState = 0;     // previous state of the button
+int d_2_state = 0;         // current state of the button
+int last_d_2_state = 0;     // previous state of the button
 
+int d_3_state = 0;         // current state of the button
+int last_d_3_state = 0;     // previous state of the button
+
+int d_4_state = 0;         // current state of the button
+int last_d_4_state = 0;     // previous state of the button
+
+
+// Analog read interval stuff
 long previousMillis = 0;
-
 long interval = 500;
 
 
 void setup() {
-  // initialize the buttbon pin as a input:
-  pinMode(buttonPin_one, INPUT);
-  pinMode(buttonPin_two, INPUT);
+  
+  pinMode(a_1, INPUT);
+  pinMode(a_2, INPUT);
+  pinMode(a_3, INPUT);
+  
+  pinMode(d_1, INPUT);
+  pinMode(d_2, INPUT);
+  pinMode(d_3, INPUT);
+  pinMode(d_4, INPUT);
+  
   // initialize the LED as an output:
   pinMode(ledPin, OUTPUT);
-
-  pinMode(LDR, INPUT);
-  //pinMode(temperaturePin, INPUT);
   
   Serial.begin(9600);
- 
 }
-
 
 void loop() {
 
   unsigned long currentMillis = millis();
-  float voltage, degreesC, degreesF;
+  float out_1, out_2, out_3; // used for analog output
 
+  // read the analog pins if it's been an appropriate amount of time
   if(currentMillis - previousMillis > interval) { 
     previousMillis = currentMillis;
-    // read the photoresister 
-    int v = analogRead(LDR);
-    Serial.print("light: ");
-    Serial.println(v); 
-
-  voltage = getVoltage(temperaturePin);
-
-  // Now we'll convert the voltage to degrees Celsius.
-  // This formula comes from the temperature sensor datasheet:
-
-  degreesC = (voltage - 0.5) * 100.0;
-
-  // While we're at it, let's convert degrees Celsius to Fahrenheit.
-  // This is the classic C to F conversion formula:
-
-  degreesF = degreesC * (9.0/5.0) + 32.0;
-
-    Serial.print("temperature: ");
-    Serial.println(degreesF);
     
+    // read all the analog pins 
+    int out_1 = analogRead(a_1);
+    int out_2 = analogRead(a_2);
+    int out_3 = analogRead(a_3);
+
+    Serial.print("a_1 ");
+    Serial.println(out_1); 
+
+    Serial.print("a_2 ");
+    Serial.println(out_2);
+    
+    Serial.print("a_3 ");
+    Serial.println(out_3);
+     
   }
+
+  // be ready to read the digital input (buttons) always --- 
   
-  // read the pushbutton input pin:
-  buttonOneState = digitalRead(buttonPin_one);
-//  buttonTwoState = digitalRead(buttonPin_two);
+  d_1_state = digitalRead(d_1);
+  d_2_state = digitalRead(d_2);
+  d_3_state = digitalRead(d_3);
+  d_4_state = digitalRead(d_4);
 
-
-  // compare the buttonState to its previous state
-  if (buttonOneState != lastButtonOneState) {
-    // if the state has changed, increment the counter
-    if (buttonOneState == HIGH) {
-      // if the current state is HIGH then the button
-      // wend from off to on:
-      buttonOnePushCounter++;
-      Serial.println("button_1: on");
-    } else {
-      // if the current state is LOW then the button
-      // wend from on to off:
-      // Serial.println("button one off");
+  // compare the current state to its previous state
+  if (d_1_state != last_d_1_state) {
+    if (d_1_state == HIGH) {
+      // if the current state is HIGH then the button went from off to on
+      Serial.println("d_1 on");
+      digitalWrite(ledPin, HIGH); // indicate with on board LED
     }
-    // Delay a little bit to avoid bouncing
-    delay(50);
+
+    delay(50); // Delay a little bit to avoid bouncing
   }
 
-//
-//  // compare the buttonState to its previous state
-//  if (buttonTwoState != lastButtonTwoState) {
-//    // if the state has changed, increment the counter
-//    if (buttonTwoState == HIGH) {
-//      // if the current state is HIGH then the button
-//      // wend from off to on:
-//      buttonTwoPushCounter++;
-//      Serial.println("button_2: on");
-//    } else {
-//      // if the current state is LOW then the button
-//      // wend from on to off:
-//      //Serial.println("button two off");
-//    }
-//    // Delay a little bit to avoid bouncing
-//    delay(50);
-//  }
-  
-  // save the current state as the last state,
-  //for next time through the loop
-  lastButtonOneState = buttonOneState;
-  //lastButtonTwoState = buttonTwoState;
+  // compare the current state to its previous state
+  if (d_2_state != last_d_2_state) {
+    if (d_2_state == HIGH) {
+      // if the current state is HIGH then the button went from off to on
+      Serial.println("d_2 on");
+      digitalWrite(ledPin, HIGH); // indicate with on board LED
+    }
 
-
-  // turns on the LED every four button pushes by
-  // checking the modulo of the button push counter.
-  // the modulo function gives you the remainder of
-  // the division of two numbers:
-  if (buttonOnePushCounter % 4 == 0) {
-    digitalWrite(ledPin, HIGH);
-  } else {
-    digitalWrite(ledPin, LOW);
+    delay(50); // Delay a little bit to avoid bouncing
   }
 
+  // compare the current state to its previous state
+  if (d_3_state != last_d_3_state) {
+    if (d_3_state == HIGH) {
+      // if the current state is HIGH then the button went from off to on
+      Serial.println("d_3 on");
+      digitalWrite(ledPin, HIGH); // indicate with on board LED
+    }
+    
+    delay(50); // Delay a little bit to avoid bouncing
+  }
+
+  // compare the current state to its previous state
+  if (d_4_state != last_d_4_state) {
+    if (d_4_state == HIGH) {
+      // if the current state is HIGH then the button went from off to on
+      Serial.println("d_4 on");
+      digitalWrite(ledPin, HIGH); // indicate with on board LED
+    }
+
+    delay(50); // Delay a little bit to avoid bouncing
+  }
+
+  // save the current state as the last state, for next time through the loop
+  last_d_1_state = d_1_state;
+  last_d_3_state = d_2_state;
+  last_d_3_state = d_3_state;
+  last_d_4_state = d_4_state;
 }
-
-
-float getVoltage(int pin)
-{ 
-  return(analogRead(pin) * 0.004882814);
-
-}
-
-
